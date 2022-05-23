c755f0a5f2bc5cb42d8986174d7c4fd246a69f26
can sardaslar <101027564+sardaslar@users.noreply.github.com>
2022-05-23 13:04:36 -0600

    Update script.js

--- Develop/script.js
+++ Develop/script.js
@@ -28,16 +28,16 @@ $(".saveBtn").on("click", function () {
 
 //Local Storage
 
-$("#hour8 .desciption").val(localStorage.getItem("hour8"));
-$("#hour9 .desciption").val(localStorage.getItem("hour9"));
-$("#hour10 .desciption").val(localStorage.getItem("hour10"));
-$("#hour11 .desciption").val(localStorage.getItem("hour11"));
-$("#hour12 .desciption").val(localStorage.getItem("hour12"));
-$("#hour13 .descipation").val(localStorage.getItem("hour13"));
-$("#hour14 .desciption").val(localStorage.getItem("hour14"));
-$("#hour15 .desciption").val(localStorage.getItem("hour15"));
-$("#hour16 .desciption").val(localStorage.getItem("hour16"));
-$("#hour17 .desciption").val(localStorage.getItem("hour17"));
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .descripation").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
 
 function hourTracker() {
     var currentHour = moment().hour();
